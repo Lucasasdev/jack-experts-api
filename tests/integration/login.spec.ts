d6a416d5850834ef57d9c50957dc5379c050a8de
Lucas Alves <lucasasdev@gmail.com>
2024-09-10 12:08:22 -0300

    test: verify datas from the response body

--- tests/integration/login.spec.ts
+++ tests/integration/login.spec.ts
@@ -18,4 +18,16 @@ describe("Login", () => {
 
     expect(response.status).toBe(401);
   });
+
+  it("Verify body values returned as response", async () => {
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "testjwt@email.com", password: "testingjwt" });
+
+    expect(response.body).toMatchObject({
+      token: expect.any(String),
+      auth: expect.any(Boolean),
+      user: expect.any(Object),
+    });
+  });
 });
